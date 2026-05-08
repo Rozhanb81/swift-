7f4e883f8d9c9bac2bcc72b4fc5f358952c4229a
rozhan bazhban <rozhaaan@rozhans-MacBook-Air.local>
2026-05-08 17:09:33 +0200

    Add first Swift project

---
+++ firstswift/firstswift/firstswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstswiftApp.swift
+//  firstswift
+//
+//  Created by rozhan bazhban on 08.05.26.
+//
+
+import SwiftUI
+
+@main
+struct firstswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
